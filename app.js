75fa83f178ae99832d6a396129dad372f8114ccd
Flor Marshall <flordmarshall@gmail.com>
2021-12-08 14:20:22 -0500

    edit app.js/ create dino.json

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-// require csvtojson module
-const CSVToJSON = require('csvtojson');
+// Node packages for file system
+var fs = require('fs');
+var path = require('path');
 
-// convert dino.csv file to JSON array
-CSVToJSON().fromFile('dino.csv')
-    .then(users => {
 
-        // users is a JSON array
-        // log the JSON array
-        console.log(dino);
-    }).catch(err => {
-        // log error if any
-        console.log(err);
-    });
+var filePath = path.join(__dirname, './dino.csv');
+// Read CSV
+var f = fs.readFileSync(filePath, {encoding: 'utf-8'}, 
+    function(err){console.log(err);});
+
+// Split on row
+f = f.split("\n");
+
+// Get first row for column headers
+headers = f.shift().split(",");
+
+var json = [];    
+f.forEach(function(d){
+    // Loop through each row
+    tmp = {}
+    row = d.split(",")
+    for(var i = 0; i < headers.length; i++){
+        tmp[headers[i]] = row[i];
+    }
+    // Add object to list
+    json.push(tmp);
+});
+
+var outPath = path.join(__dirname, './dino.json');
+// Convert object to string, write json to file
+fs.writeFileSync(outPath, JSON.stringify(json), 'utf8', 
+    function(err){console.log(err);});
